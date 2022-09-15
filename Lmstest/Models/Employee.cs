5588bfe7f74b0c35e11de591509878d9221d35c3
ShrutiL2 <shrutilahoti111@gmail.com>
2022-09-15 13:41:58 +0530

    lms final project

---
+++ Lmstest/Models/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Lmstest.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            InverseManager = new HashSet<Employee>();
+            Leaves = new HashSet<Leave>();
+        }
+
+        public int Empid { get; set; }
+
+        [Required(ErrorMessage = "name invalid"), MaxLength(10), MinLength(3)]
+        [Display(Name = "Employee Name")]
+        public string Empname { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Email")]
+        public string Empemail { get; set; }
+
+        [Display(Name = "Department")]
+        public string Department { get; set; }
+        [Display(Name = "Address")]
+        public string Empaddress { get; set; }
+
+        [Required(ErrorMessage = "phone number invalid"), MaxLength(12), MinLength(3)]
+        [Display(Name = "Phone Number")]
+        public string Phnumber { get; set; }
+
+        [Display(Name = "Username")]
+        public string Empusername { get; set; }
+
+
+        [Display(Name = "Password")]
+        
+        public string Emppassword { get; set; }
+
+        [Display(Name = "Leaves In Hand")]
+        public int? Leaveinhand { get; set; }
+
+        [Display(Name = "Manager Name")]
+        public int? Managerid { get; set; }
+
+        
+
+        public virtual Employee Manager { get; set; }
+        public virtual ICollection<Employee> InverseManager { get; set; }
+        public virtual ICollection<Leave> Leaves { get; set; }
+    }
+}
---
+++ Lmstest/Models/Leave.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Lmstest.Models
+{
+    public partial class Leave : IValidatableObject
+    {
+        public int Leaveid { get; set; }
+        public int? Empid { get; set; }
+
+        public int? Manid { get; set; }
+
+        [Required(ErrorMessage = "The start date is required")]
+        [Display(Name = "Start Date:")]
+        public DateTime? Leavefrom { get; set; }
+
+        [Required(ErrorMessage = "The end date is required")]
+        [Display(Name = "End Date:")]
+        public DateTime? Leaveto { get; set; }
+
+
+        public int? Noofdays { get; set; }
+        public string Leavestatus { get; set; }
+        public string Leavetype { get; set; }
+
+        public virtual Employee Emp { get; set; }
+
+        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
+        {
+            if (Leaveto < Leavefrom)
+            {
+                yield return new ValidationResult("EndDate must be greater than StartDate");
+                // return Ok("invalid date");
+
+            }
+        }
+    }
+}
